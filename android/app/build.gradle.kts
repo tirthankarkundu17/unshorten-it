fe82c3eaa005c4acfca7675f7873263f1935ffc8
Tirthankar Kundu <tirthankar.learning@gmail.com>
2026-03-07 11:03:37 +0530

    Update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "in.bitmaskers.unshortenit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
