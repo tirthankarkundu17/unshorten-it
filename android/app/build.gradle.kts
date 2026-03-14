d422d01444c136f546473ba140d855464ace65fc
Tirthankar Kundu <tirthankar.learning@gmail.com>
2026-03-14 16:07:56 +0530

    feat: Add initial Android application build configuration with dynamic backend URL and AdMob settings.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "in.bitmaskers.unshortenit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
