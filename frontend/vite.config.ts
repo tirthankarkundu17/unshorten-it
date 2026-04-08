3408ed9cdb2655ac14d6eb997ad32b62eea8fa8e
Tirthankar Kundu <tirthankar.learning@gmail.com>
2026-04-08 14:39:08 +0530

    chore: update pwa to prompt updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['logo.svg'],
       manifest: {
         name: 'Unshorten IT',
